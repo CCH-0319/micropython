bd849d064b790e3a52db679bc9a6642a5130aeac
Damien George <damien@micropython.org>
2025-06-11 13:32:16 +1000

    stm32/powerctrl: Move N6 standby init to board control function.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/stm32/powerctrl.c
+++ ports/stm32/powerctrl.c
@@ -26,10 +26,10 @@
 
 #include "py/mperrno.h"
 #include "py/mphal.h"
+#include "boardctrl.h"
 #include "powerctrl.h"
 #include "rtc.h"
 #include "extmod/modbluetooth.h"
-#include "py/mpconfig.h"
 #ifndef NO_QSTR
 #include "genhdr/pllfreqtable.h"
 #endif
@@ -1074,13 +1074,6 @@ NORETURN void powerctrl_enter_standby_mode(void) {
     // Clear all WKUPx flags.
     LL_PWR_ClearFlag_WU();
 
-    // Upon wake from standby, jump to the code at SRAM1.
-    // LL_PWR_EnableTCMSBRetention();
-    // LL_PWR_EnableTCMFLXSBRetention();
-    // LL_APB4_GRP2_EnableClock(LL_APB4_GRP2_PERIPH_SYSCFG);
-    // SCB_CleanDCache();
-    // extern uint32_t iram_bootloader_isr_vector;
-    // SYSCFG->INITSVTORCR = (uint32_t)&iram_bootloader_isr_vector;
     #else
 
     // We need to clear the PWR wake-up-flag before entering standby, since
