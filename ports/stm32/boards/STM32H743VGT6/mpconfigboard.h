847955529dfbecbd26252a577d2568b296f76605
CHUNG CHIH-HUNG <56349177+CCH-0319@users.noreply.github.com>
2025-10-08 10:37:20 +0800

    Update mpconfigboard.h

--- ports/stm32/boards/STM32H743VGT6/mpconfigboard.h
+++ ports/stm32/boards/STM32H743VGT6/mpconfigboard.h
@@ -42,20 +42,11 @@ extern void board_enter_bootloader(void);
 #define MICROPY_BOARD_ENTER_BOOTLOADER(nargs, args) board_enter_bootloader()
 
 // Note these are not used in top system.c.
-
-#define MICROPY_HW_CLK_PLLM         (3)     // 12/3=4 MHz
-#define MICROPY_HW_CLK_PLLN         (200)   // 4*200=800 MHz (VCO)
-#define MICROPY_HW_CLK_PLLP         (2)     // 800/2=400 MHz SYSCLK
-#define MICROPY_HW_CLK_PLLQ         (8)     // 800/8=100 MHz (USB)
-#define MICROPY_HW_CLK_PLLR         (2)     // 800/2=400 MHz
-
-/*
 #define MICROPY_HW_CLK_PLLM         (5)     // 25/5=5 MHz
 #define MICROPY_HW_CLK_PLLN         (192)   // 5*192=960 MHz (VCO)
 #define MICROPY_HW_CLK_PLLP         (2)     // 960/2=480 MHz SYSCLK
-#define MICROPY_HW_CLK_PLLQ         (20)    // 960/20=48 MHz (USB)
+#define MICROPY_HW_CLK_PLLQ         (10)    // 960/10=96 MHz (USB)
 #define MICROPY_HW_CLK_PLLR         (2)     // 960/2=480 MHz
-*/
 
 // UART1 config
 #define MICROPY_HW_UART1_TX  (pin_B14)
@@ -135,9 +126,9 @@ extern const struct _mp_obj_type_t mod_network_nic_type_winc;
 #define MICROPY_HW_USB_PID_CDC2_MSC_HID         (MICROPY_HW_USB_PID)
 #define MICROPY_HW_USB_PID_CDC3_MSC_HID         (MICROPY_HW_USB_PID)
 #define MICROPY_HW_USB_LANGID_STRING            0x409
-#define MICROPY_HW_USB_MANUFACTURER_STRING      "OpenMV"
-#define MICROPY_HW_USB_PRODUCT_FS_STRING        "OpenMV Virtual Comm Port in FS Mode"
-#define MICROPY_HW_USB_PRODUCT_HS_STRING        "OpenMV Virtual Comm Port in HS Mode"
+#define MICROPY_HW_USB_MANUFACTURER_STRING      "CC"  //"OpenMV"
+#define MICROPY_HW_USB_PRODUCT_FS_STRING        "CC Virtual Comm Port in FS Mode"  //"OpenMV Virtual Comm Port in FS Mode"
+#define MICROPY_HW_USB_PRODUCT_HS_STRING        "CC Virtual Comm Port in HS Mode"  //"OpenMV Virtual Comm Port in HS Mode"
 #define MICROPY_HW_USB_INTERFACE_FS_STRING      "VCP Interface"
 #define MICROPY_HW_USB_INTERFACE_HS_STRING      "VCP Interface"
 #define MICROPY_HW_USB_CONFIGURATION_FS_STRING  "VCP Config"
